66fa60150ab82fd39814e320c83c2f4cd99c2236
Stefany Cárdenas Martínez <st.cardenas@udla.edu.co>
2020-07-05 09:28:34 -0500

    Simplementó el confirmar contraseña

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -1,4 +1,5 @@
 import 'package:drumsapp2/src/bloc/login_bloc.dart';
+import 'package:drumsapp2/src/bloc/signUp_bloc.dart';
 export 'package:drumsapp2/src/bloc/login_bloc.dart';
 
 import 'package:flutter/material.dart';
@@ -6,6 +7,7 @@ import 'package:flutter/material.dart';
 class Provider extends InheritedWidget{
 
   final loginBloc = LoginBloc();
+  final signUpBloC = SignUpBloc();
 
   Provider ({Key key, Widget child})
     : super(key: key, child: child);
@@ -13,9 +15,13 @@ class Provider extends InheritedWidget{
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
 
-  static LoginBloc of(BuildContext context) {
+  static LoginBloc ofL(BuildContext context) {
     // ignore: deprecated_member_use
     return (context.inheritFromWidgetOfExactType(Provider) as Provider).loginBloc;
+  }
+  static SignUpBloc ofS(BuildContext context) {
+    // ignore: deprecated_member_use
+    return (context.inheritFromWidgetOfExactType(Provider) as Provider).signUpBloC;
   }
 
 }
